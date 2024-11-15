e2f16a215931a4b6ebbcfd21d315499e178e9636
Matúš Koterba <matus.koterba@gmail.com>
2024-11-15 11:24:42 +0100

    Base structure

---
+++ src/LaravelExporterServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Matusboa\LaravelExporter;
+
+use Illuminate\Support\ServiceProvider;
+
+final class LaravelExporterServiceProvider extends ServiceProvider
+{
+    //
+}
